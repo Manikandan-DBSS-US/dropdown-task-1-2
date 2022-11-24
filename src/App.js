07f1105b59ff2a0eaa09d716334c6ed6b2722705
unknown <you@example.com>
2022-11-24 14:50:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Dropdown_task_1 } from './Dropdown_task_1';
+import { Dropdown_task_2 } from './Dropdown_task_2';
+
+function App() {
+  return (
+    <div>
+      <Dropdown_task_1 />
+      <Dropdown_task_2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dropdown_task_2.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Select from 'react-select'
+
+export const Dropdown_task_2 = () => {
+    const [certificate, setCertificate] = useState([]);
+    const [loading, isLoading] = useState(true);
+
+    const getCertificates = async () => {
+        const { data } = await axios.get('https://securityjobapioverturedev.azurewebsites.net/api/Dropdown/getspecialties/1');
+        const arr = []
+        data.data.map(item => {
+            arr.push({ value: item.id, label: item.name },)
+        })
+        setCertificate(arr);
+        isLoading(false);
+    }
+
+    useEffect(() => {
+        getCertificates();
+    }, [])
+
+    return (
+        <div className="container">
+            <div className="row mt-3">
+                <h5>TASK - 2</h5>
+                <div className="col d-flex gap-3">
+                    <Select id="dropdown-box" className="select"
+                        classNamePrefix="select" isMulti options={certificate} />
+                    <div> {loading && <div class="spinner-border" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
